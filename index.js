52038f225901b860704bffee13512b359756f727
vivien anglesio <vivien.anglesio@gmail.com>
2017-05-09 23:09:34 +0200

    wip

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
+'use strict';
+const config = require ('./lib/config');
+
 module.exports = {
-    Remoted: require('./lib/remote/remoted')
-,   User: require('./lib/remote/remoted')
-,   cache: require('./lib/cache')
+    config: function (cfg)
+    {
+        config.Client = cfg.Client || require ('./lib/models/client');
+        config.User = cfg.User || require ('./lib/models/user');
+        config.httpServer = cfg.httpServer || createDefaultHttpServer(config.httpPort || 80);
+        config.io = cfg.io || require('socket.io')(config.httpServer, cfg.ioOptions);
+        config.path = cfg.path || '';
+        attachServer(config.path, config.httpServer);
+    }
+,   Remoted: require('./lib/remote/remoted')
+,   cache: require('./lib/remote/cache')
 ,   errors: {
         NotFoundError: require('./lib/errors/not-found-error')
     }
+,   endpoint: require('./lib/remote/remote-room').register
 };
+
+function createDefaultHttpServer()
+{
+    let server = require('http').createServer();
+    server.listen(port);
+    return server
+}
+
+function attachServer(path, srv)
+{
+    const url = path + '/remoted.js';
+    const urlMap = path + '/remoted.js.map';
+    const evs = srv.listeners('request').slice(0);
+
+    srv.removeAllListeners('request');
+    srv.on('request', (req, res) => {
+        if (0 === req.url.indexOf(urlMap)) {
+            self.serveMap(req, res);
+        } else if (0 === req.url.indexOf(url)) {
+            self.serve(req, res);
+        } else {
+            for (var i = 0; i < evs.length; i++) {
+                evs[i].call(srv, req, res);
+            }
+        }
+    });
+}
 
